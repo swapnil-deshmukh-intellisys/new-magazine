8de047ee6d01835c2a92b8dac24e6714e647cce5
ritesh-intellisys <ritesh.intellisys@gmail.com>
2025-12-08 13:14:42 +0530

    sanity cors

--- src/config/sanity.js
+++ src/config/sanity.js
@@ -6,7 +6,7 @@ export const SANITY_CONFIG = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "gaa41xdq",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-03",
-  useCdn: process.env.NEXT_PUBLIC_SANITY_USE_CDN === "true" || false,
+  useCdn: process.env.NEXT_PUBLIC_SANITY_USE_CDN !== "false", // Enable CDN by default to help with CORS
 };
 
 // Sanity Content Types
